a5dabbd463860b518aa944a5288e614f094749dc
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-14 22:56:45 -0300

    Testing Kivy

--- HiKivy.py
+++ HiKivy.py
@@ -1,15 +1,15 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class MainApp(App):
+class HiKivy(App):
     def build(self):
-        label = Label(text = 'Hello World',
+        label = Label(text = 'Hi Kivy!',
                       size_hint = (.5, .5),
                       pos_hint = {'center_x': .5, 'center_y': .5})
         return label
 
 if __name__ == '__main__':
-    app = MainApp()
+    app = HiKivy()
     app.run()
 
 
